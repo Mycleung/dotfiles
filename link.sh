371baeaa4d1409633cd24a4b9aeee1491f41485b
Michael Leung <>
2018-09-19 11:22:01 +0100

    Update linker script to only link files

--- link.sh
+++ link.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 for dotfile in $DIR/.*[a-z]; do
-  [ -e "$dotfile" ] || continue
+  [ -f "$dotfile" ] || continue
   ln -s $dotfile ~
 done
 
